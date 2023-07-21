af0af687b451b0b5db752b643e8298b086c621df
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-07-21 11:59:27 +0530

    gg

---
+++ Scripts/index.js
@@ -0,0 +1,22 @@
+import { ParcelContainer, HeaderContainer,Data } from "./Components.js"
+
+
+
+// 
+const DupData=[...Data]
+
+
+// Getting Html Elements
+
+const header=document.getElementById("header")
+const parcel=document.getElementById("parcelContainer")
+
+
+
+
+// appending Data in Dom
+
+
+console.log(DupData,Data);
+console.log(ParcelContainer());
+console.log(HeaderContainer())
